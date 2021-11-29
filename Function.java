ba43f1a14d8aada41e29a6774aee8f63abe818b6
Agapitos Oikonomidis <t8200127@aueb.gr>
2021-11-30 01:29:27 +0200

    Update Function.java

--- Function.java
+++ Function.java
@@ -9,14 +9,13 @@ public class Function {
 			for(int j = 0; j <= 3; j++) {
 				System.out.println(GameApp.tablep[j].getPlayerName()+" is your turn to play"); 
 				
+				// checkSoldiers()
 				
-				
-				
-				
+				// placeSoldiers()
 				
 				
 				System.out.println("From where do you want to attack ?"); // Attack
-				String tempColor = GameApp.tablep[j].getPlayerColor(); // Attacker's color ( temporary )
+				String tempColor = GameApp.tablep[j].getPlayerColor(); 
 				ArrayList<String> alliedStates = new ArrayList<String>();
 				for(int k = 0; k <= 19; k++) {
 					if(tempColor.equals(GameApp.tabler[k].getRegionColor())) {
@@ -25,35 +24,71 @@ public class Function {
 					}
 				}
 				
-				boolean flag = true; // Check valid input
-				while(flag) {
-					String ra = keyboard.nextLine();
+				String ras = null;
+				boolean flag = true; 
+				while(flag) { // Check valid input
+					ras = keyboard.nextLine();
 					for(int counter = 0; counter <= alliedStates.size(); counter++) {
-						if(ra.equals(alliedStates.get(counter))) {
+						if(ras.equals(alliedStates.get(counter))) {
 							flag = false;
 							break;
 						}
-					}
-								
+					}							
 				}
+				
+				int ra = 0; // Save attacker's index in tabler[]
+				for(int counter = 0; counter <= 19; counter++) {
+					if(ras.equals(GameApp.tabler[counter].getRegionName())) {
+						ra = counter;
+						break;
+					}	
+				}				
+				System.out.println("Where do you want to attack ?");
+				System.out.println(GameApp.tabler[ra].getBorders()); //Print borders of region tabler[ra]
+				
+				String rds = null;
+				boolean flag2 = true;
+				while(flag2) { // Check valid input
+					rds = keyboard.nextLine();	
+					for(int counter = 0; counter <= 19; counter++) {
+						if(true) {
+							flag2 = false;
+							break;
+						}
+					}
+				}
+				
+				int rd = 0; // Save defender's index in tabler[]
+				for(int counter = 0; counter <= 19; counter++) {
+					if(rds.equals(GameApp.tabler[counter].getRegionName())) {
+						rd = counter;
+						break;
+					}
+				} // End of attack
+				
+				attack(ra,rd);
+				
+				// attack()
+				
+				// fortify()
 			}
 		}
 	}
 	
-	public void checkSoldiers() { // Gives soldiers to players depending on their regions' number //
+	public static void checkSoldiers() { // Gives soldiers to players depending on their regions' number //
 		
 	}
 	
-	public void placeSoldiers() { // Players choose where to place their soldiers //
+	public static void placeSoldiers(int s) { // Players choose where to place their soldiers //
 		
 	}
 	
-	public void attack(Region ra, Region rd) { // The option of attack ( from where to where; ) //
+	public static void attack(int ra, int rd) { // The option of attack ( from where to where; ) //
 		
 		
 	}
 	
-	public void fortify() { // The option of fortifying soldiers ( from where; to where; how much; ) //
+	public static void fortify() { // The option of fortifying soldiers ( from where; to where; how much; ) //
 	
 	}
 		
